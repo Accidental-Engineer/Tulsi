680fb320eea155bb7985ff0628229c1c4b4c5436
Tarun Kumar <tarun12.tarunkr@gmail.com>
2018-03-14 02:59:26 +0530

    chat.php test success
    chat.php working with test data

--- test.php
+++ test.php
@@ -1,25 +1,44 @@
 <?php
-
+// start_session();
 require_once 'vendor/autoload.php';
-
 use GraphAware\Neo4j\Client\ClientBuilder;
-
 $client = ClientBuilder::create()
     ->addConnection('bolt', 'bolt://neo4j:123@localhost:7687')
     ->build();
-$query = "MATCH (s:symptom)-[:has_symptom]-(q:question)-[:has_option]->(o:option) with q.q as questions ,collect(o) as options return questions , options limit 1000";
-$result = $client->run($query);
 
-foreach ($result->getRecords() as $record) {
-  //print_r($record);
-    echo sprintf('Question is : %s  options', $record->value('questions'));
-    echo "<br>";
-    $options = $record->value('options');
-    foreach ( $options as $key => $val) {
-      print_r($record->value('options')[$key]->value('val'));
-      echo "<br>";
-    }
-    echo "<br>";
-    // print_r($record->value('options')[]->value('val'));
+if (isset($_POST['userResponse']) && $_POST['userResponse'] !="") {
+  if ($_POST['id']=="-1") {
+    $q_id = "match (s:SYMPTOM)-[:OPTION]->(q:QUESTION) return ID(q) as id";
+    $result_id = $client->run($q_id);
+    $id = $result_id->getRecord()->value('id');
+  }
+  else {
+    $id = $_POST['id'];
+    $q_id = "match (:QUESTION)-[r:OPTION]->(q:QUESTION) where ID(r) =".$id." return ID(q) as id";
+    $result_id = $client->run($q_id);
+    $id = $result_id->getRecord()->value('id');
+    //echo $id;
+  }
 
+  $q = "match (q:QUESTION) where ID(q)=".$id." return q.value as question";
+  $o = "match (q0:QUESTION)-[r:OPTION]->(q1:QUESTION) where ID(q0)=".$id." return ID(r) as id, r.value as options";
+  $result_q = $client->run($q);
+  $result_o = $client->run($o);
+
+  $JSON_responce = "{\"question\":";
+
+  foreach ($result_q->getRecords() as $record_q) {
+      $question=$record_q->value('question');
+      $options="{";
+      $i = 1;
+      foreach ( $result_o->getRecords() as $record_o) {
+        if($i!=1) $options .= "," ;
+        $options .= "\"".$record_o->value('options')."\":".$record_o->value('id')."";
+        $i++;
+      }
+      $options .="}";
+  $JSON_responce .= "\"".$question."\"".",\"options\":".$options."}";
+  echo $JSON_responce;
+  // print_r($record->value('options')[]->value('val'));
+  }
 }?>
