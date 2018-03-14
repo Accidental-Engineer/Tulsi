5eaf0d1ab94d16e070d21f02c7f3ce28170817e6
Shivam Kumar <skshivam64@users.noreply.github.com>
2018-03-14 14:05:51 +0530

    Useless changes

--- test.php
+++ test.php
@@ -5,21 +5,23 @@ require_once 'vendor/autoload.php';
 use GraphAware\Neo4j\Client\ClientBuilder;
 
 $client = ClientBuilder::create()
-    ->addConnection('bolt', 'bolt://neo4j:123@localhost:7687')
+    ->addConnection('bolt', 'bolt://neo4j:9955040194@localhost:7687')
     ->build();
-$query = "MATCH (s:symptom)-[:has_symptom]-(q:question)-[:has_option]->(o:option) with q.q as questions ,collect(o) as options return questions , options limit 1000";
+$query = "MATCH (s:SYMPTOM)-[r:OPTION]->(q:QUESTION)-[t:OPTION]->(m:QUESTION) RETURN s, r, q, t, m";
 $result = $client->run($query);
 
+print_r($result);
+
 foreach ($result->getRecords() as $record) {
-  //print_r($record);
-    echo sprintf('Question is : %s  options', $record->value('questions'));
-    echo "<br>";
-    $options = $record->value('options');
-    foreach ( $options as $key => $val) {
+    //echo sprintf('Question is : %s  options', $record->value('questions'));
+    //echo "<br>";
+    //$options = $record->value('options');
+    foreach ( $record as $key => $val) {
       print_r($record->value('options')[$key]->value('val'));
       echo "<br>";
     }
-    echo "<br>";
-    // print_r($record->value('options')[]->value('val'));
+    //print_r($record);
+    echo "<br><br>";
+}
 
-}?>
+?>
